916f9ff23f9a7eb157f863b57fe388696806aaef
H2KKI2 <36642902+H2KKI2@users.noreply.github.com>
2018-06-11 00:13:44 +0200

    Update password reset
    Co-Authored-By: mixxamm <mixxamm@users.noreply.github.com>

--- password_reset_script.php
+++ password_reset_script.php
@@ -23,28 +23,40 @@ $result = mysqli_query($conn, $uid_query);
 $row = mysqli_fetch_assoc($result);
 $uid = $row['user_id'];
 
+if($uid !=""){
 
-$date = date("Y-m-d H:i:s", strtotime('+20 minutes'));
+	$date = date("Y-m-d H:i:s", strtotime('+20 minutes'));
 
-$password_reset_query = "insert into password_reset (reset_ID, expiration_date, uid) VALUES('$reset_ID', '$date', '$uid');";
-mysqli_query($conn, $password_reset_query);
+	$password_reset_query = "insert into password_reset (reset_ID, expiration_date, uid) VALUES('$reset_ID', '$date', '$uid');";
+	mysqli_query($conn, $password_reset_query);
 
-    $headers = 'From: ' . $from . "\r\n" .
-    'Reply-To: ' . $to . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+		$headers = 'From: ' . $from . "\r\n" .
+		'Reply-To: ' . $to . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+	mail($to, $subject, $message, $headers);
 
-echo "<script>
-swal({
-title: 'Check je mail!',
-text: 'We hebben je een bericht gestuurd waarmee je je wachtwoord kan resetten!',
-type: 'success'
-}).then(function() {
-window.location.href = 'https://vadaza.be';
-});
-</script>";
-
+	echo "<script>
+	swal({
+	title: 'Check je mail!',
+	text: 'We hebben je een bericht gestuurd waarmee je je wachtwoord kan resetten!',
+	type: 'success'
+	}).then(function() {
+	window.location.href = 'https://vadaza.be';
+	});
+	</script>";
+}
+else{
+	echo "<script>
+	swal({
+	title: 'Oeps!',
+	text: 'We vinden geen gebruiker met de e-mail: $to in ons systeem!',
+	type: 'error'
+	}).then(function() {
+	window.location.href = 'https://vadaza.be/password_reset.php';
+	});
+	</script>";
+}
 
 ?>
 </body>
